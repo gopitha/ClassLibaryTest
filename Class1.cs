984ac8790d94f1c949e50220c7c2d40700a7e226
gopitha <gopitha@live.no>
2020-06-30 10:17:35 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibaryTest
+{
+    public class Foo
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello world! From a dll");
+        }
+
+
+    }
+}
